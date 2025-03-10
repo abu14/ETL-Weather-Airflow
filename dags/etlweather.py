97f67a4afeab757c607aec6474973bc2e942f112
abu14 <tesfayeabenezer64@gmail.com>
2025-03-10 19:38:57 +0300

    module 'dags/etlweather.py' updated

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -3,7 +3,8 @@ from airflow.providers.https.hooks.http import HttpHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
-
+import requests
+import json
 
 # Latitude and Longiture
 LATITUDE = '51.5074'
